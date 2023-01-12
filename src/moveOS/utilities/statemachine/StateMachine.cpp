1c072ee7aa2a9f146d8ee39ceaa5195308de4a84
usama <mahmoodify@gmail.com>
2023-01-12 16:06:02 +0500

    adding statemachine

---
+++ src/moveOS/utilities/statemachine/StateMachine.cpp
@@ -0,0 +1,1 @@
+#include "utilities/statemachine/StateMachine.h"
---
+++ src/moveOS/utilities/statemachine/StateMachine.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "base/Task.h"
+
+
+namespace moveOS
+{
+  namespace kernel
+  {
+
+    class StateMachine : public moveOS::base::Task
+    {
+    };
+
+  }
+}
