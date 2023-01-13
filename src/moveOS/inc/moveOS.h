e098548b62501cf7a6b840dbc6cde31243d86898
usama <mahmoodify@gmail.com>
2023-01-13 11:45:54 +0500

    adding combinational moveOS.h

---
+++ src/moveOS/inc/moveOS.h
@@ -0,0 +1,18 @@
+#ifndef _MOVE_OS_INC_M_EXECUTION_MANAGER_H_
+#define _MOVE_OS_INC_M_EXECUTION_MANAGER_H_
+
+
+
+#include "inc/MConfigurationProvider.h"
+#include "inc/MEvent.h"
+#include "inc/MEventsDispatcher.h"
+#include "inc/MExecutionManager.h"
+#include "inc/MState.h"
+#include "inc/MStateMachine.h"
+#include "inc/MTask.h"
+#include "inc/MThreadTask.h"
+#include "inc/MVarsUpdater.h"
+
+
+
+#endif
