c76f2bd2f61586fdb5ab92c65738bdf1bbb368a9
usama <mahmoodify@gmail.com>
2023-01-14 19:36:38 +0500

    adding platforms

--- src/moveOS/inc/_config.h
+++ src/moveOS/inc/_config.h
@@ -2,8 +2,28 @@
 #define _MOVE_OS_INC_CONFIG_H_
 
 
-
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/* Version info                                                               */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 #define MOVE_OS_VERSION   1
+
+
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/* Target platform                                                            */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#define PLATFORM_WINDOWS
+#define PLATFORM_LINUX
+#define PLATFORM_ARDUINO
+#define PLATFORM_STM32F4
+
+#ifndef TARGET_PLATFORM
+#define TARGET_PLATFORM PLATFORM_LINUX
+#endif
 
 
 
