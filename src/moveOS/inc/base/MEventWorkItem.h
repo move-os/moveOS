d92b9fa695d10bcaf8b54bceed71b4a2a5c41181
usama <mahmoodify@gmail.com>
2023-01-14 20:56:44 +0500

    adding event work item

---
+++ src/moveOS/inc/base/MEventWorkItem.h
@@ -0,0 +1,21 @@
+#ifndef _MOVE_OS_INC_BASE_M_EVENT_WORK_ITEM_H_
+#define _MOVE_OS_INC_BASE_M_EVENT_WORK_ITEM_H_
+
+#include "_config.h"
+
+
+namespace moveOS
+{
+  namespace base
+  {
+
+    class MEventWorkItem
+    {
+    };
+
+  }
+}
+
+
+
+#endif
---
+++ src/moveOS/src/base/EventWorkItem.cpp
@@ -0,0 +1,3 @@
+#include "inc/base/MEventWorkItem.h"
+
+#include "src/_internal_inc/macros.h"
