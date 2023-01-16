8089ac54f992ba7a2f5e80299bb7c8690eb0231d
usama <mahmoodify@gmail.com>
2023-01-16 18:05:19 +0500

    updating the platform list

--- src/moveOS/inc/_config.h
+++ src/moveOS/inc/_config.h
@@ -19,12 +19,15 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 #define PLATFORM_GNU_LINUX              0x00010000
-#define PLATFORM_WINDOWS                0x00020000
-#define PLATFORM_WINDOWS_OR_LINUX       PLATFORM_WINDOWS | PLATFORM_GNU_LINUX
-#define PLATFORM_FREE_RTOS              0x00040000
-#define PLATFORM_AZURE_RTOS             0x00080000
-#define PLATFORM_BAREMETAL_ARDUINO      0x00100000
-#define PLATFORM_BAREMETAL_STM32F4      0x00200000
+#define PLATFORM_MAC_OS                 0x00020000
+#define PLATFORM_WINDOWS                0x00040000
+
+#define PLATFORM_FREE_RTOS              0x00100000
+#define PLATFORM_AZURE_RTOS             0x00200000
+
+#define PLATFORM_BAREMETAL_ARDUINO      0x01000000
+#define PLATFORM_BAREMETAL_STM32F       0x02000000
+#define PLATFORM_BAREMETAL_STM32H       0x04000000
 
 #ifndef TARGET_PLATFORM
 #define TARGET_PLATFORM PLATFORM_GNU_LINUX
