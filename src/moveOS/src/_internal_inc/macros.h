01fda912b5fb9a38ee4b9cb9b1d7028b4cd6a5b5
usama <mahmoodify@gmail.com>
2023-02-04 23:11:29 +0500

    adding trimming macros

--- src/moveOS/src/_internal_inc/macros.h
+++ src/moveOS/src/_internal_inc/macros.h
@@ -5,5 +5,13 @@
 
 
 
+#define STR_RTRIM(x) {size_t __inr_=0;while(x[__inr_]!=' ' && x[__inr_]!='\0')__inr_++;x[__inr_]='\0';}
+#define STR_LTRIM(x) {size_t __nsl_=0,__inl_=0;while(x[__nsl_]==' ')__nsl_++;while(x[__nsl_]!='\0'){x[__inl_]=x[__nsl_];__inl_++;__nsl_++;}x[__inl_]='\0';}
+#define STR_TRIM(x) STR_LTRIM(x) STR_RTRIM(x)
+
+
+
+
+
 
 #endif
