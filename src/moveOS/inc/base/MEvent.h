34e4726b0759112ea1493740d8ed54cae02914c4
usama <mahmoodify@gmail.com>
2023-02-04 22:58:58 +0500

    adding polling event

--- src/moveOS/inc/base/MEvent.h
+++ src/moveOS/inc/base/MEvent.h
@@ -11,17 +11,34 @@ namespace moveOS
   namespace base
   {
 
-    class MEvent
+    /*********************************************************************************/
+    /*                                                                               */
+    /* Base Class for all the events.                                                */
+    /*                                                                               */
+    /*   execute() is invoked periodically to check for events.                      */
+    /*       It is essential for events that need to be checked through polling.     */
+    /*                                                                               */
+    /*********************************************************************************/
+    class MEvent {
+    public:
+      virtual void execute(single elapsedTime_sec) = 0;
+    };
+
+
+
+
+
+    class MPollingEvent : public MEvent
     {
     public:
+      MPollingEvent(event_poll_func pollFunction, MByteDataEventWorkItem *firstWorkItem);
+      void addNextWorkItem(MByteDataEventWorkItem *nextWorkItem);
+      void execute(single elapsedTime_sec) override;
 
-      virtual MEvent* thenPassTo(MSimplestEventWorkItem) = 0;
 
-
-
-    private:
-
-      MSimplestEventWorkItem* firstWorkItem;
+    protected:
+      event_poll_func pollingFunction;
+      MByteDataEventWorkItem* firstWorkItem;
     };
 
   }
--- src/moveOS/src/base/Event.cpp
+++ src/moveOS/src/base/Event.cpp
@@ -1,3 +1,39 @@
 #include "inc/base/MEvent.h"
 
 #include "src/_internal_inc/macros.h"
+
+
+moveOS::base::MPollingEvent::MPollingEvent(event_poll_func pollFunction, MByteDataEventWorkItem *firstWorkItem)
+{
+  this->pollingFunction = pollFunction;
+  this->firstWorkItem = firstWorkItem;
+}
+
+void moveOS::base::MPollingEvent::addNextWorkItem(MByteDataEventWorkItem* nextWorkItem)
+{
+  if (firstWorkItem != nullptr && nextWorkItem != nullptr)
+  {
+    MByteDataEventWorkItem* n = firstWorkItem;
+
+    while (n->nextWorkItem != nullptr)
+    {
+      n = n->nextWorkItem;
+    }
+
+    n->nextWorkItem = nextWorkItem;
+  }
+}
+
+void moveOS::base::MPollingEvent::execute(single elapsedTime_sec)
+{
+  if (this->pollingFunction != nullptr && firstWorkItem != nullptr)
+  {
+    byte ret = this->pollingFunction();
+
+    if (ret != -1)
+    {
+      firstWorkItem->executeChain(ret);
+    }
+  }
+}
+
