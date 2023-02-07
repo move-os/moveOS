f5f492f9c1bd6bda362204a1272f88a39d34fcbd
usama <mahmoodify@gmail.com>
2023-02-07 12:51:25 +0500

    implementing logging passing over

--- src/moveOS/src/base/LoggerSplit.cpp
+++ src/moveOS/src/base/LoggerSplit.cpp
@@ -36,19 +36,35 @@ moveOS::base::MLoggerSplit::MLoggerSplit(
   logger_04 = fourthLogger;
 }
 
-void moveOS::base::MLoggerSplit::log(log_level, const char* message)
+void moveOS::base::MLoggerSplit::log(log_level logLevel, const char* message)
 {
+  if (logger_01 != nullptr) logger_01->log(logLevel, message);
+  if (logger_02 != nullptr) logger_02->log(logLevel, message);
+  if (logger_03 != nullptr) logger_03->log(logLevel, message);
+  if (logger_04 != nullptr) logger_04->log(logLevel, message);
 }
 
 void moveOS::base::MLoggerSplit::logInfo(const char* message)
 {
+  if (logger_01 != nullptr) logger_01->logInfo(message);
+  if (logger_02 != nullptr) logger_02->logInfo(message);
+  if (logger_03 != nullptr) logger_03->logInfo(message);
+  if (logger_04 != nullptr) logger_04->logInfo(message);
 }
 
 void moveOS::base::MLoggerSplit::logWarning(const char* message)
 {
+  if (logger_01 != nullptr) logger_01->logWarning(message);
+  if (logger_02 != nullptr) logger_02->logWarning(message);
+  if (logger_03 != nullptr) logger_03->logWarning(message);
+  if (logger_04 != nullptr) logger_04->logWarning(message);
 }
 
 void moveOS::base::MLoggerSplit::logError(const char* message)
 {
+  if (logger_01 != nullptr) logger_01->logError(message);
+  if (logger_02 != nullptr) logger_02->logError(message);
+  if (logger_03 != nullptr) logger_03->logError(message);
+  if (logger_04 != nullptr) logger_04->logError(message);
 }
 
