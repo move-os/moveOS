2aa43cd67a4faeac56caae700dfe2feaed929679
usama <mahmoodify@gmail.com>
2023-02-07 22:04:03 +0500

    updating to add private ctor and logger

--- src/moveOS/inc/kernel/MExecutionManager.h
+++ src/moveOS/inc/kernel/MExecutionManager.h
@@ -4,6 +4,9 @@
 #include "_types.h"
 #include "_config.h"
 
+#include "base/MObjectProvider.h"
+#include "base/MLogger.h"
+
 
 namespace moveOS
 {
@@ -12,6 +15,16 @@ namespace moveOS
 
     class MExecutionManager
     {
+    public:
+
+
+
+    private:
+      MExecutionManager(moveOS::base::MLogger*);
+
+      moveOS::base::MLogger* logger;
+
+      friend class MObjectProvider;
     };
 
   }
