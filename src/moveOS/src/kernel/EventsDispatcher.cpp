cc24066460dcc0187a99ecafc75ae45f297bf908
usama <mahmoodify@gmail.com>
2023-02-07 22:06:48 +0500

    impl ctor - only saving logger

--- src/moveOS/src/kernel/EventsDispatcher.cpp
+++ src/moveOS/src/kernel/EventsDispatcher.cpp
@@ -1,3 +1,9 @@
 #include "inc/kernel/MEventsDispatcher.h"
 
 #include "src/_internal_inc/macros.h"
+
+
+moveOS::kernel::MEventsDispatcher::MEventsDispatcher(moveOS::base::MLogger* logger)
+{
+  this->logger = logger;
+}
