13636c9f8437b14d34ee3259c7fcacbe20a1bc0f
usama <mahmoodify@gmail.com>
2023-02-08 19:17:24 +0500

    updating include guard

--- src/moveOS/inc/utilities/MCommTcpClient.h
+++ src/moveOS/inc/utilities/MCommTcpClient.h
@@ -1,5 +1,5 @@
-#ifndef _MOVE_OS_INC_UTILITIES_M_COMM_TCP_H_
-#define _MOVE_OS_INC_UTILITIES_M_COMM_TCP_H_
+#ifndef _MOVE_OS_INC_UTILITIES_M_COMM_TCP_CLIENT_H_
+#define _MOVE_OS_INC_UTILITIES_M_COMM_TCP_CLIENT_H_
 
 #include "_types.h"
 #include "_config.h"
