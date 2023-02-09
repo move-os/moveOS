f9540c9d5a72668217036ae3ca30e5bd34dbcbd9
usama <mahmoodify@gmail.com>
2023-02-09 22:19:00 +0500

    adding blank impl funcs

--- src/moveOS/src/utilities/comm/CommUdpClient.cpp
+++ src/moveOS/src/utilities/comm/CommUdpClient.cpp
@@ -1,3 +1,44 @@
 #include "inc/utilities/MCommUdpClient.h"
 
 #include "src/_internal_inc/macros.h"
+
+
+moveOS::utilities::comm::MCommUdpClient::MCommUdpClient(
+  word localPort = MUdpPort::ANY,
+  bool enableBroadcasting = false,
+  bool isNonBlocking = false)
+{
+
+}
+
+moveOS::utilities::comm::MCommUdpClient::~MCommUdpClient()
+{
+}
+
+bool moveOS::utilities::comm::MCommUdpClient::IsSocketCreated()
+{
+  return false;
+}
+
+bool moveOS::utilities::comm::MCommUdpClient::IsSocketBound()
+{
+  return false;
+}
+
+void moveOS::utilities::comm::MCommUdpClient::Close()
+{
+}
+
+bool moveOS::utilities::comm::MCommUdpClient::IsPacketAvailable(word timeoutSecond, word timeoutMicrosecond)
+{
+  return false;
+}
+
+void moveOS::utilities::comm::MCommUdpClient::SendMessage(const byte* buffer, word numBytes, const byte* targetIP, word targetPort)
+{
+}
+
+packet_info moveOS::utilities::comm::MCommUdpClient::ReceiveMessage(byte* buffer, word buffSize)
+{
+  return packet_info();
+}
