f320b73351ad1c58f2d6a776ec0d81e6226fef8b
usama <mahmoodify@gmail.com>
2023-02-11 20:29:32 +0500

    using ellipsis version of functions

--- src/moveOS/inc/base/MLogger.h
+++ src/moveOS/inc/base/MLogger.h
@@ -15,15 +15,15 @@ namespace moveOS
     public:
       void setMinimumLogLevel(log_level);
 
-      virtual void log(log_level, const char* message) = 0;
-      virtual void logInfo(const char* message) = 0;
-      virtual void logWarning(const char* message) = 0;
-      virtual void logError(const char* message) = 0;
+      virtual void log(log_level, const char* message, ...) = 0;
+      virtual void logInfo(const char* message, ...) = 0;
+      virtual void logWarning(const char* message, ...) = 0;
+      virtual void logError(const char* message, ...) = 0;
 
-      virtual void logNoEndline(log_level, const char* message) = 0;
-      virtual void logInfoNoEndline(const char* message) = 0;
-      virtual void logWarningNoEndline(const char* message) = 0;
-      virtual void logErrorNoEndline(const char* message) = 0;
+      virtual void logNoEndline(log_level, const char* message, ...) = 0;
+      virtual void logInfoNoEndline(const char* message, ...) = 0;
+      virtual void logWarningNoEndline(const char* message, ...) = 0;
+      virtual void logErrorNoEndline(const char* message, ...) = 0;
 
     protected:
       MLogger();
