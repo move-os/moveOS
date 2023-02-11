b811276263599b818ec969377cbe174188141194
usama <mahmoodify@gmail.com>
2023-02-11 21:37:13 +0500

    updating signatures and var name

--- src/moveOS/inc/base/MLoggerSplit.h
+++ src/moveOS/inc/base/MLoggerSplit.h
@@ -19,9 +19,14 @@ namespace moveOS
       MLoggerSplit(MLogger*, MLogger*, MLogger*, MLogger*);
 
       void log(log_level, const char* message) override;
-      void logInfo(const char* message) override;
-      void logWarning(const char* message) override;
-      void logError(const char* message) override;
+      void logInfo(const char* format_str, ...) override;
+      void logWarning(const char* format_str, ...) override;
+      void logError(const char* format_str, ...) override;
+
+      void logNoEndline(log_level, const char* message) override;
+      void logInfoNoEndline(const char* format_str, ...) override;
+      void logWarningNoEndline(const char* format_str, ...) override;
+      void logErrorNoEndline(const char* format_str, ...) override;
 
     private:
       MLogger* logger_01;
