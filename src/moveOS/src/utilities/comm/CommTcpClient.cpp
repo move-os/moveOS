1fe4cd8771d7de73e09ec464796433e855a918e8
usama <mahmoodify@gmail.com>
2023-02-14 20:02:28 +0500

    impl connection of socket

--- src/moveOS/src/utilities/comm/CommTcpClient.cpp
+++ src/moveOS/src/utilities/comm/CommTcpClient.cpp
@@ -30,6 +30,82 @@ bool moveOS::utilities::comm::MCommTcpClient::Connect()
 {
   if (isConnected == false)
   {
+    if (socketFileDescriptor == -1)
+    {
+      socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+
+      if (socketFileDescriptor == -1)
+      {
+        logger->logError("Socket cannot be created for TCP Client connecting with %s:%d", targetIpAddress, targetPort);
+        return false;
+      }
+    }
+
+    if (inet_addr((const char*)targetIpAddress) == -1)
+    {
+      struct hostent* he;
+      struct in_addr** addr_list;
+
+      if ((he = gethostbyname((const char*)targetIpAddress)) == NULL)
+      {
+        logger->logError("Failed to resolve the hostname: %s", targetIpAddress);
+        return false;
+      }
+
+      addr_list = (struct in_addr**)he->h_addr_list;
+
+      for (int i = 0; addr_list[i] != NULL; i++)
+      {
+        targetSocketAddress.sin_addr = *addr_list[i];
+        break;
+      }
+    }
+    else
+    {
+      targetSocketAddress.sin_addr.s_addr = inet_addr((const char*)targetIpAddress);
+    }
+
+    targetSocketAddress.sin_family = AF_INET;
+    targetSocketAddress.sin_port = htons(targetPort);
+
+    if (connect(socketFileDescriptor,
+                (struct sockaddr*)&targetSocketAddress,
+                sizeof(targetSocketAddress)) < 0)
+    {
+      logger->logError("Unable to connect to %s:%d", targetIpAddress, targetPort);
+      return false;
+    }
+
+    if (isNonBlocking)
+    {
+
+#if   TARGET_PLATFORM == PLATFORM_GNU_LINUX
+      int status = fcntl(socketFileDescriptor, F_SETFL, fcntl(socketFileDescriptor, F_GETFL, 0) | O_NONBLOCK);
+
+      if (status == -1)
+      {
+        isNonBlocking = false;
+        this->logger->logWarning("Unable to set TCP connection with %s:%d as non-blocking", targetIpAddress, targetPort);
+      }
+
+
+#elif   TARGET_PLATFORM == PLATFORM_WINDOWS
+      u_long mode = 1;
+      int nonBlockingModeResult = ioctlsocket(socketFileDescriptor, FIONBIO, &mode);
+
+      if (nonBlockingModeResult == SOCKET_ERROR)
+      {
+        this->logger->logWarning("Unable to set TCP connection with %s:%d as non-blocking", targetIpAddress, targetPort);
+      }
+
+#else
+#error "Cannot set socket to non-blocking for selected platform"
+
+#endif
+      
+    }
+
+    isConnected = true;
   }
 
   return isConnected;
