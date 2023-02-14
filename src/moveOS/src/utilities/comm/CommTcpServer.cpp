61987348b6c57c73175857a4e90e342c9385ec97
usama <mahmoodify@gmail.com>
2023-02-14 22:57:39 +0500

    impl initial ctor

--- src/moveOS/src/utilities/comm/CommTcpServer.cpp
+++ src/moveOS/src/utilities/comm/CommTcpServer.cpp
@@ -2,3 +2,27 @@
 
 #include "src/_internal_inc/macros.h"
 
+#include "inc/base/MLogger.h"
+
+
+moveOS::utilities::comm::MCommTcpServer::MCommTcpServer(moveOS::base::MLogger* logger)
+{
+  this->logger = logger;
+  this->socketFileDescriptor = -1;
+
+
+#if   TARGET_PLATFORM == PLATFORM_WINDOWS
+
+
+  WSADATA wsaData;
+  int windowsStartupResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (windowsStartupResult != 0)
+  {
+    this->logger->logError("WSAStartup failed with return code: %d", windowsStartupResult);
+    return;
+  }
+
+
+#endif
+}
+
