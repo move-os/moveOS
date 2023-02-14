1db453ac15d9fb5d1c55cc215da0ab554d93afd7
usama <mahmoodify@gmail.com>
2023-02-14 19:03:01 +0500

    adding headers for windows and linux

--- src/moveOS/inc/utilities/MCommTcpServer.h
+++ src/moveOS/inc/utilities/MCommTcpServer.h
@@ -1,8 +1,48 @@
 #ifndef _MOVE_OS_INC_UTILITIES_M_COMM_TCP_SERVER_H_
 #define _MOVE_OS_INC_UTILITIES_M_COMM_TCP_SERVER_H_
 
-#include "_types.h"
 #include "_config.h"
+
+
+#if   TARGET_PLATFORM == PLATFORM_GNU_LINUX
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+
+
+#elif   TARGET_PLATFORM == PLATFORM_WINDOWS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <fcntl.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+
+
+
+#else
+
+//- 
+
+
+
+
+#endif
+
+
+#include "_types.h"
 
 
 namespace moveOS
