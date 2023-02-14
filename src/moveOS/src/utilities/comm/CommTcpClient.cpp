d7560612a0c51bd6db23b2ad78c49c5562ffa300
usama <mahmoodify@gmail.com>
2023-02-14 19:25:12 +0500

    implementing the ctor

--- src/moveOS/src/utilities/comm/CommTcpClient.cpp
+++ src/moveOS/src/utilities/comm/CommTcpClient.cpp
@@ -1,3 +1,20 @@
 #include "inc/utilities/MCommTcpClient.h"
 
 #include "src/_internal_inc/macros.h"
+
+
+moveOS::utilities::comm::MCommTcpClient::MCommTcpClient(moveOS::base::MLogger* logger,
+                                                        byte* targetIpAddress, word targetPort,
+                                                        bool isNonBlocking = true)
+{
+  this->logger = logger;
+
+  this->targetIpAddress = targetIpAddress;
+  this->targetPort = targetPort;
+
+  this->isNonBlocking = isNonBlocking;
+
+  this->isConnected = false;
+
+  this->socketFileDescriptor = -1;
+}
